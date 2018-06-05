fab467b2725b167aeb6729ac3f2df20e58ed67fb
Jacob Logas <jakeman893@ufl.edu>
2018-06-05 17:41:16 -0400

    Add basis for UI

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
 from business_rules import run_all
 from rules import rules_json
 from models import *
+from tkinter import *
 
-rules = rules_json
+class Application(Frame):
+    def app_run_all(self):
+        run_all(rule_list=self.rules,
+            defined_variables=SongVariables(self.song),
+            defined_actions=SongActions(self.song),
+            stop_on_first_trigger=False
+            )
+
+    def createWidgets(self):
+        self.QUIT               = Button(self)
+        self.QUIT["text"]       = "QUIT"
+        self.QUIT["fg"]         = "red"
+        self.QUIT["command"]    = self.quit
+
+        self.QUIT.pack({"side": "left"})
+
+        self.run_rules = Button(self)
+        self.run_rules["text"] = "Run Rules"
+        self.run_rules["command"] = self.app_run_all
+
+        self.run_rules.pack({"side": "left"})
+
+    def __init__(self, song, rules, master=None):
+        Frame.__init__(self, master)
+        self.song = song
+        self.rules = rules
+        self.pack()
+        self.createWidgets()
 
 example = {
     "descriptors": ["grounded", "melodic", "acoustical"],
@@ -15,12 +43,9 @@ example = {
     "genre": None,
 }
 
-song = example
-
-run_all(rule_list=rules,
-    defined_variables=SongVariables(song),
-    defined_actions=SongActions(song),
-    stop_on_first_trigger=False
-    )
+root = Tk()
+app = Application(master=root, song=example, rules=rules_json)
+app.mainloop()
+root.destroy()
 
 print(example["genre"])
